bb6ec72bc2bc46effe18efb934382279bf57b7d1
Danny Guzman <guzmanjd86@gmail.com>
2018-09-12 15:27:07 -0700

    Corrected API calls and create UI

--- data/search.php
+++ data/search.php
@@ -1,4 +1,4 @@
 <form>
-<p>Keywords:<input type="text" name="keywords"></p>
-<input type="submit" value="Search">
+<p>Keywords: <input type="text" name="keywords"><span class="ca-gov-icon-search"><input type="submit" value="Search"></span></p>
+
 </form>
--- index.php
+++ index.php
@@ -4,9 +4,6 @@
 session_start();
 include("core/init.php");
 
-if (isset($_GET['appID'])) {
-    retrieveApp($_GET['appID']);
-}
 ?>
 <!doctype html>
 <html class="no-js" lang="en">
@@ -31,15 +28,74 @@ if (isset($_GET['appID'])) {
 			<!-- Begin Main Content -->
 			<div class="section">
 				<div class="container">
+					<!-- Search Archive Form -->
 					<div class="full-width">
-						<?php include('data/search.php'); ?>
-						<div>
-							<?php print_r($_POST); ?>
-						</div>
+            <form id="archivesearchform" method="GET">
+            <p>Search: <input type="text" name="keywords"> <input type="submit"><span class="ca-gov-icon-search"></span></p>
+            <p><label><input type="radio" name="sin" value="" <?= ! isset($_GET['sin']) || empty($_GET['sin']) ? 'checked="checked"' : '' ?>> Search Metadata</label>
+              <label><input type="radio" name="sin" value="TXT" <?= isset($_GET['sin']) && 'TXT' == $_GET['sin'] ? 'checked="checked"' : '' ?>> Search text contents</label>
+              <label><input type="radio" name="sin" value="web" <?= isset($_GET['sin']) && 'web' == $_GET['sin'] ? 'checked="checked"' : '' ?>> Search archived web sites</label>
+            </p>
+            </form>
 					</div>
+					<!-- End of Search Archive Form -->
+					<!-- Search Results -->
+					<div class="full-width">
+						<?php 
+						
+							if(isset($_GET['keywords']) && ! empty(trim($_GET['keywords']))){
+								$q = strtolower(trim($_GET['keywords']));
+								$q = -1 < strpos($q, 'california') ? $q : "$q California";
+								
+								$sin = $_GET['sin'];
+								
+								if('TXT' !== $sin ){
+									$wayback_url = sprintf('https://archive.org/advancedsearch.php?q=%1$s&output=json', $q );
+									
+								}else{
+									$wayback_url = sprintf('services/search/v1/scrape?fields=title&q=%1$s&output=json', $q );
+									
+								}
+								
+								$wayback_img_url = "https://archive.org/services/img/";
+								$wayback_detail_url = "https://archive.org/details/";
+								
+								$ch = curl_init();
+								curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+								curl_setopt($ch, CURLOPT_URL, $wayback_url);
+								curl_exec($ch);
+
+								// if no errors using the Google Geocoding API
+								if ( ! curl_errno($ch)) {
+									$response = json_decode( curl_multi_getcontent($ch))->response->docs;
+									print_r( get_object_vars( $response[1] ) );
+									
+									foreach( $response as $r => $res):
+										$title = $res->title;
+										$identifier = $res->identifier;
+										$date = (new DateTime($res->date) )->format('M j, Y');
+										
+										$img = $wayback_img_url . $identifier;
+										$detail_link = $wayback_detail_url . $identifier;
+										?>
+										<div class="third text-center">
+											<div><img src="<?= $img ?>" alt="<?= $res->title ?>" style="max-height:120px;"/></div>
+											<a href="<?= $detail_link ?>"><?= $title ?></a>
+											<p class="published"><?= $date ?></p>
+										</div>
+									<?php endforeach; 
+							}else{
+								print_r( curl_error($ch) );
+							}
+								
+							}
+						?>
+					</div>
+					<!-- Search Results -->
+					
 				</div>
-				<!-- End Main Content -->
 			</div>
+			<!-- End Main Content -->
 		</main>
 	</div>
 
@@ -47,3 +103,21 @@ if (isset($_GET['appID'])) {
 
 </body>
 </html>
+<style>
+#archivesearchform span{
+	font-size:32px;
+	vertical-align:middle;
+	margin-left:-32px;
+}
+#archivesearchform input[name="keywords"]{
+	width:90%;
+}
+#archivesearchform input[type="submit"]{
+	width:32px;
+	opacity:0;
+}
+label{
+	cursor: pointer;
+	padding-right: 15px;
+}
+</style>
