0952442856c3105831c53e357d7ad24459a2f992
Danny Guzman <guzmanjd86@gmail.com>
2018-09-24 11:08:09 -0700

    Added Year filters to results If not already in search query, append edmund brown

--- index.php
+++ index.php
@@ -8,12 +8,10 @@ include("core/init.php");
 <head>
 	<meta charset="utf-8">
 	<title>California Archives</title>
-	<?php include("ssi/head.html"); ?>
-	<link rel="stylesheet" href="style.css" />
+	<!--#include virtual="ssi/head.html" -->
 </head>
 <body class="primary">
-	<?php include('header.php'); ?>
-
+	<?php include("ssi/header.html"); ?>
 	<div id="main-content" class="main-content">
 		<main class="main-primary">
 			<!-- Begin Page Title -->
@@ -30,14 +28,25 @@ include("core/init.php");
 					<form id="archivesearchform" method="GET">
 						<div class="full-width">
 							<p>Search: <input type="text" name="keywords" value="<?= $keyword  ?>"> <input type="submit"><span class="ca-gov-icon-search"></span></p>
-							<p><label><input type="radio" name="sin" value="" <?= empty($sin) ? 'checked="checked"' : '' ?>> Search Metadata</label>
+							<p><label><input type="radio" name="sin" value="metadata" <?= 'metadata' == $sin ? 'checked="checked"' : '' ?>> Search Metadata</label>
 								<label><input type="radio" name="sin" value="tv" <?= 'tv' == $sin ? 'checked="checked"' : '' ?>> Search TV News Captions</label>
 							</p>
 						</div>
 						<?php if( ! empty($output) ): ?>
-							<div class="quarter">
-								
+							<h4><?= $result_count ?> Results Found</h4>
+							<div class="quarter filters">
+								<?php if( ! empty($years) ) : ?>
+									<h5>Year</h5>
+									<ul>
+								<?php foreach($years as $y => $year): ?>
+									<li><label><input name="year[]" type="checkbox" value="<?= $y ?>"<?= in_array($y, $filter_years) ? ' checked="checked"' : ''?>> <?= $y ?></label><i class="pull-right"><?= $year ?></i></li>
+								<?php endforeach; ?>
+								</ul>
+							<?php endif; ?>
+							<div class="text-center"><input class="filter btn btn-primary " type="submit" value="Apply Filters"></div>
 							</div>
+						<?php elseif(empty($output) && ! empty($keyword) ): ?>
+							<h4>No Results Found</h4>
 						<?php endif; ?>
 					</form>
 					<!-- End of Search Archive Form -->
@@ -60,25 +69,3 @@ include("core/init.php");
 
 </body>
 </html>
-<style>
-#archivesearchform span{
-	font-size:32px;
-	vertical-align:middle;
-	margin-left:-32px;
-}
-#archivesearchform input[name="keywords"]{
-	width:90%;
-}
-#archivesearchform input[type="submit"]{
-	width:32px;
-	opacity:0;
-}
-.third{
-	max-height: 180px;
-	min-height: 180px;
-}
-label{
-	cursor: pointer;
-	padding-right: 15px;
-}
-</style>
